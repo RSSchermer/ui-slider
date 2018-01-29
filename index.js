af92166f1f7f94de87b88cc36bb1fcb6801c96dd
Roland <roland0507@gmail.com>
2018-01-29 12:44:01 +0100

    Fix funnel paths

--- index.js
+++ index.js
@@ -19,13 +19,13 @@ module.exports = {
     var parentApp = (typeof app.import !== 'function' && app.app) ? app.app : app;
     var options = extend(defaultOptions, app.options['ui-slider']);
 
-    parentApp.import('bower_components/seiyria-bootstrap-slider/dist/bootstrap-slider.js');
+    parentApp.import('vendor/bootstrap-slider.js');
 
     if (options.importBootstrapSliderCSS) {
-      parentApp.import('bower_components/seiyria-bootstrap-slider/dist/css/bootstrap-slider.css');
+      parentApp.import('vendor/bootstrap-slider.css');
     }
     if (options.importAddonCss) {
-      parentApp.import('vendor/ui-slider/ui-slider.css');
+      parentApp.import('vendor/ui-slider.css');
     }
   },
 
